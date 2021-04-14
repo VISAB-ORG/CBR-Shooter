d2f657ce8d816416633030167eeb0ea4f8382266
VocalTrance <platenpaypal@gmail.com>
2021-04-14 19:25:39 +0200

    Adds initial VISABConnector project

---
+++ VISABConnector/src/Attribute/DontSerialize.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VISABConnector
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class DontSerialize : Attribute
+    {
+        public DontSerialize()
+        {
+        }
+    }
+}
---
+++ VISABConnector/src/VISABConnection.cs
@@ -0,0 +1,22 @@
+﻿namespace VISABConnector
+{
+    public class VISABConnection
+    {
+        private const uint VISABPORT = 9999;
+
+        public static VISABConnection Connect()
+        {
+            // Bla bla bla connect to visab, falls positive rueckmeldung => Geb connection object
+            // zurueck andernfalls mal gucken
+
+            return new VISABConnection();
+        }
+
+        public bool SendStatistics<T>(T statistics) where T : IVISABStatistics
+        {
+            // Send the statistics
+
+            return default;
+        }
+    }
+}
